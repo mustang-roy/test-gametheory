2b4ce4c842ea81387da8b67c0fff02ad5e229734
Mustang <lfsmariz@gmail.com>
2020-02-15 12:18:22 -0300

    edit main test

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from include.players import Player
 from include.game import Game
 
-bad_player = Player(True, 'bad')
-fool_player = Player(True, 'fool')
-raging_player = Player(True, 'raging')
-pardoner_player = Player(True, 'pardon')
-swindler_player = Player(True, 'swindler')
+bad_player = Player(False, 'bad', 100000, 0)
+fool_player = Player(True, 'fool', 100000, 100000)
+raging_player = Player(True, 'raging', 100000, 10000)
+pardoner_player = Player(True, 'pardon', 100000, 100000)
+swindler_player = Player(True, 'swindler', 100000, 100000)
+
+game1 = Game(fool_player, fool_player)
+
+game1.play_game()
+
+print(game1.player_1_coin)
+print(game1.player_2_coin)
+
+
 
 
 
